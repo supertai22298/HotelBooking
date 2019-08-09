ff814922a52f2c8f4b76a7b5e98e0a8ddf630819
Nguyen Chiem Hao <nguyenchiemhao2205@gmail.com>
2019-08-09 10:51:56 +0700

    third-edit grid left side bar :))

--- resources/views/page/hotel-grid-left-sidebar.blade.php
+++ resources/views/page/hotel-grid-left-sidebar.blade.php
@@ -8,7 +8,20 @@
         
         
         <!--=================== PAGE-COVER =================-->
-      @include('page.components.h-page-cover')
+        <section class="page-cover" id="cover-hotel-grid-list">
+          <div class="container">
+              <div class="row">
+                  <div class="col-sm-12">
+                    <h1 class="page-title">Hotel Grid Left Sidebar</h1>
+                      <ul class="breadcrumb">
+                          <li><a href="#">Home</a></li>
+                          <li class="active">Hotel Grid Left Sidebar</li>
+                      </ul>
+                  </div><!-- end columns -->
+              </div><!-- end row -->
+          </div><!-- end container -->
+        </section><!-- end page-cover -->
+        
       
       <!--===== INNERPAGE-WRAPPER ====-->
       <section class="innerpage-wrapper">
