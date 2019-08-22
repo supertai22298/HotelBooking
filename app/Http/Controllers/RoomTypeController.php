72c386e22678fc18fb5c4956923d0776355ab3b7
TeiiPlz <supertai22298@gmail.com>
2019-08-22 16:06:08 +0700

    không dùng repository nữa

--- app/Http/Controllers/RoomTypeController.php
+++ app/Http/Controllers/RoomTypeController.php
@@ -3,21 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\RoomType;
-use App\Repositories\RoomType\RoomTypeRepository;
+use App\RoomType;
 
 class RoomTypeController extends Controller
 {
-    //
-    protected $roomTypeRepository;
-
-    public function __construct(RoomTypeRepository $roomTypeRepository)
-    {
-        $this->roomTypeRepository = $roomTypeRepository;
-    }
-
+    
     public function create(Request $request){
         $this->roomTypeRepository->create($request->all());
-         return redirect('/');
+        return redirect('/');
     }
 }
