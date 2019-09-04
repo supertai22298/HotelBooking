fc22f029463c6fa8ef6bb0f3875bb98d6092056d
Nguyen Chiem Hao <nguyenchiemhao2205@gmail.com>
2019-09-04 23:16:06 +0700

    sửa route adminlogin

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -100,7 +100,7 @@
 
                   <a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
 
-              <a class="nav-link" href="{{route('logout')}}"><i class="fa fa-power -off"></i>Logout</a>
+              <a class="nav-link" href="{{route('get-admin-logout')}}"><i class="fa fa-power -off"></i>Logout</a>
               </div>
           </div>
 
