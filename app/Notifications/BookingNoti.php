e7ea2bd62ee392d2f618ac802da7850c0e3ec437
Pham Anh Tai <phamanhtai263@gmail.com>
2019-09-11 11:24:05 +0700

    bi sida

---
+++ app/Notifications/BookingNoti.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class BookingNoti extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->line('Bạn vừa mới đặt phòng tại website hata! Xin vui lòng giữ máy chờ nhân viên bên chúng tôi liên lạc cho bạn!')
+                    ->action('Notification Action', url('/'))
+                    ->line('Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            'data'=>'Đây là noti'
+        ];
+    }
+}
